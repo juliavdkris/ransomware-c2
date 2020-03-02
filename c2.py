340328f713c3f421af0939e1eb889e95cdcd1d6d
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2020-03-02 17:20:25 +0100

    Add getkey API

--- c2.py
+++ c2.py
@@ -1,21 +1,30 @@
-from bottle import post, request, HTTPError, run
+from bottle import get, post, request, HTTPError, run
 import json
 
 import config
 
 
 @post('/sendkey')
-def index():
+def sendkey():
 	if request.json:
 		uuid = request.json['uuid']
 		key = request.json['key']
 
-		with open('keys.json', ) as file:
+		with open('keys.json') as file:
 			keys = json.load(file)
 		keys[uuid] = {'key': key}
 		with open('keys.json', 'w') as file:
 			json.dump(keys, file)
 
+
+@get('/getkey')
+def getkey():
+	if request.query:
+		uuid = request.query['uuid']
+
+		with open('keys.json') as file:
+			keys = json.load(file)
+		return keys[uuid]
 
 
 run(host='localhost', port=8000, reloader=True, debug=config.DEBUG, interval=0.1)
